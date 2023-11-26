00df261ff78bc3c8ca634336b2b929ab27ae70f4
Smetankin <smet-eb@yandex.ru>
2023-11-26 21:31:14 +0300

    made README.md, fixed send_greeting function

--- library_test_work/library/service.py
+++ library_test_work/library/service.py
@@ -2,5 +2,4 @@ from django.core.mail import send_mail
 
 
 def send_greeting(user_email):
-    # send_mail("new letter", "Greeting!!!", "library@gmail.com", [user_email], fail_silently=False)
-    print(f'this is mail for "{user_email}"!')
+    send_mail("new letter", "Greeting!!!", "library@gmail.com", [user_email], fail_silently=False)
